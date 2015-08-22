0fabb67a59a5b6bdeb5405c5a50583bd850c9d4c
mbjoseph <maxwellbjoseph@gmail.com>
2015-08-22 13:45:25 -0600

    added a hierarchical model for snail prevalence, to later combine with a model of snail density to get an estimate of infected snail density that propagates uncertainty throughout

--- R/isd.stan
+++ R/isd.stan
@@ -4,94 +4,84 @@ data {
   int<lower=0> nregion;
   int<lower=0> y[n];
   int<lower=1, upper=nsite> site[n];
-  int<lower=1> nspec;
-  int<lower=1, upper=nspec> h_spec[n];
-  int<lower=1, upper=nregion> region[n];
+  int<lower=1, upper=nregion> region[nsite];
   int<lower=1> nyear;
   int<lower=1, upper=nyear> year[n];
   // covariates
-  vector[n] local_richness;
-  vector[n] region_richness;
+  vector[n] richness;
   // snail data
-  vector[n] isd; // infected snail density
-}
-
-transformed data {
-  vector[n] isd2;
-  
-  isd2 <- isd .* isd;
+  int nsnail;
+  int<lower=0> y_snail[nsnail]; // number of infected snails
+  int<lower=0> k_snail[nsnail]; // number of snails tested
+  int<lower=1, upper=nsite> site_snail[nsnail]; // site belonging of above
 }
 
 parameters {
   real a0;
   vector[nsite] eta_siteR;
   real<lower=0> sigma_site;
-  vector[nregion] eta_regionR;
-  real<lower=0> sigma_region;
-  vector[nspec] eta_speciesR;
-  real<lower=0> sigma_species;
   vector[n] eta_indivR;
   real<lower=0> sigma_indiv;
   vector[nyear] eta_year;
-  real beta_local;
-  real beta_region;
-  real beta_isd;
-  real beta_intxn;
+  real beta_rich;
+  real mu_p;
+  vector[nsite] p_siteR;
+  real<lower=0> sigma_p;
+  vector[nregion] p_regionR;
+  real<lower=0> sigma_p_region;
 }
 transformed parameters {
   vector[nsite] eta_site;
-  vector[nregion] eta_region;
-  vector[nspec] eta_species;
   vector[n] eta_indiv;
   vector[n] log_mu;
+  vector[nsnail] logit_p;
+  vector[nsite] mu_p_site;
+  vector[nregion] p_region;
+  vector[nsite] p_site;
   
   eta_site <- eta_siteR * sigma_site;
-  eta_region <- eta_regionR * sigma_region;
-  eta_species <- eta_speciesR * sigma_species;
   eta_indiv <- eta_indivR * sigma_indiv;
+  p_region <- p_regionR * sigma_p_region;
+
 
 { // temp scope
   vector[n] site_eff;
-  vector[n] spec_eff;
-  vector[n] reg_eff;
   vector[n] year_eff;
   for (i in 1:n){
     site_eff[i] <- eta_site[site[i]];
-    spec_eff[i] <- eta_species[h_spec[i]];
-    reg_eff[i] <- eta_region[region[i]];
     year_eff[i] <- eta_year[year[i]];
   }
-  log_mu <- site_eff + reg_eff
-              + spec_eff + year_eff 
-              + a0
-              + beta_local * local_richness 
-              + beta_isd * isd
-              + beta_intxn * isd .* local_richness
-              + beta_region * region_richness + eta_indiv;
+  log_mu <- a0 + site_eff + year_eff + eta_indiv;
 }
+
+  for (i in 1:nsite){
+    mu_p_site[i] <- p_region[region[site_snail[i]]];
+  }
+    
+  p_site <- mu_p_site + p_siteR * sigma_p;
+  
+  for (i in 1:nsnail){
+    logit_p[i] <- mu_p + p_site[site_snail[i]];
+  }
+    
 }
 
 model {
+  // amphib Rib parameters
   a0 ~ normal(0, 3);
-  beta_local ~ normal(0, 3);
-  beta_region ~ normal(0, 3);
-  beta_isd ~ normal(0, 3);
-  beta_intxn ~ normal(0, 3);
+  beta_rich ~ normal(0, 3);
   sigma_site ~ normal(0, 3);
-  sigma_region ~ normal(0, 1);
-  sigma_species ~ normal(0, 3);
   eta_siteR ~ normal(0, 1);
-  eta_regionR ~ normal(0, 1);
-  eta_speciesR ~ normal(0, 1);
   eta_year ~ normal(0, 1);
   eta_indivR ~ normal(0, 1);
-  y ~ poisson_log(log_mu);
-}
 
-generated quantities {
-  vector[n] log_lik;
+  // snail prevalence parameters
+  sigma_p ~ normal(0, 3);
+  sigma_p_region ~ normal(0, 2);
+  p_regionR ~ normal(0, 1);
+  p_siteR ~ normal(0, 1);
   
-  for (i in 1:n){
-    log_lik[i] <- poisson_log_log(y[i], log_mu[i]);
-  }
+  // likelihood
+  y ~ poisson_log(log_mu);
+  y_snail ~ binomial_logit(k_snail, logit_p);
 }
--- R/model_fitting.R
+++ R/model_fitting.R
@@ -1,44 +1,169 @@
 # fitting a simple random effect model to the rib abundance data
-source('R/d_clean_joint.R')
+source('R/d_clean.R')
 source('R/helpers.R')
 library(rstan)
+library(maps)
+library(scales)
 
+## Define spatial neighbors -----------------------
+agg_dist <- 5
+nbs <- dnearneigh(s_coord, d1 = 0, d2 = agg_dist, longlat = T)
+par(mfrow=c(1, 1))
+plot(nbs, coords=s_coord)
+(neighborhoods <- n.comp.nb(nbs))
+num_neigh <- neighborhoods$nc
+coord_d$nbhood <- neighborhoods$comp.id
+plot(coord_d$Lon, coord_d$Lat, col=coord_d$nbhood)
+map('county', add=T, col=alpha(1, .3))
+map('state', add=T)
+
+all(levels(pd$fsite) == levels(coord_d$site))
+pd$numsite <- as.numeric(pd$fsite)
+pd$region <- coord_d$nbhood[pd$numsite]
+region <- coord_d$nbhood[order(coord_d$site)]
+
+# Calculate apparent regional host richness ----------------------------
+host_cols <- c("amca", "bubo", "psre", "raca", "radr", "tagr", "tato")
+nyear <- length(unique(pd$year))
+
+all(levels(snails$fsite) == levels(pd$fsite))
+snails$num_site <- as.numeric(snails$fsite)
+snails$region <- region[snails$num_site]
+
+snails$reg_rich <- NA
+for (i in 1:max(snails$region)){
+  assmtyears <- unique(snails$AssmtYear_1[snails$region == i])
+  if (length(assmtyears) == 0){
+    print(paste('No assessment years for region', i))
+    next
+  }
+  for (j in 1:length(assmtyears)){
+    indx <- which(snails$region == i & 
+                    snails$AssmtYear_1 == assmtyears[j])
+    if (length(indx) == 0){
+      print(paste('length of indx = 0 for i, j', i, j, sep=' '))
+    }
+    subd <- snails[indx, host_cols]
+    seen_at_all <- apply(subd, 2, FUN=function(x) sum(x) > 0)
+    snails$reg_rich[indx] <- sum(seen_at_all)
+  }
+}
+
+# Add regional richness data from snails to pd ----------------------
+snails$reg_year <- paste(snails$region, snails$AssmtYear_1, sep='.')
+pd$helisoma_density <- snails$`HELI_ SW_1`[match(pd$fsite, snails$fsite)]
+pd$heli_rib_prevalence <- snails$HELIRIB_3[match(pd$fsite, snails$fsite)]
+pd$ISD <- pd$helisoma_density * pd$heli_rib_prevalence
+plot(jitter(snails$reg_rich), jitter(snails$local_rich))
+pd$reg_year <- paste(pd$region, pd$year, sep='.')
+pd$reg_rich <- snails$reg_rich[match(pd$reg_year, snails$reg_year)]
+plot(jitter(pd$reg_rich), jitter(pd$local_rich))
+
+pd <- pd[!is.na(pd$reg_rich), ]
+
+# Summarize Rib data by region & year ------------------------------
+library(plyr)
+summary_d <- ddply(pd, c("region", 'year'), 
+                   summarise, 
+                   rib_pres = as.numeric(any(rib > 0) | any(ISD > 0)),
+                   reg_rich = unique(reg_rich),
+                   mean_ISD = mean(ISD),
+                   sd_ISD = sd(ISD),
+                   nsampled = length(site), 
+                   mean_rib = mean(rib))
+summary_d
+plot(summary_d$nsampled, summary_d$reg_rich)
+cc <- complete.cases(summary_d[c('mean_ISD', 'sd_ISD')])
+cor(summary_d$mean_ISD[cc], summary_d$sd_ISD[cc])
+summary_d$reg_year <- paste(summary_d$region, summary_d$year, sep='.')
+pd$mean_ISD <- summary_d$mean_ISD[match(pd$reg_year, summary_d$reg_year)]
+
+# Subset data to locations where Rib detected --------------------------
+rib_occ <- ddply(pd, c('fsite', 'year'), summarize, 
+                 rib_occ=as.numeric(any(rib > 0) | any(ISD > 0)))
+rib_occ$fsiteyear <- paste(rib_occ$fsite, rib_occ$year, sep='.')
+pd$fsiteyear <- paste(pd$fsite, pd$year, sep='.')
+pd$rib_occ <- rib_occ$rib_occ[match(pd$fsiteyear, rib_occ$fsiteyear)]
+
+pd$fsiteyear <- as.factor(pd$fsiteyear)
+snails$fsiteyear <- factor(paste(snails$fsite, snails$AssmtYear_1, sep='.'))
+all(levels(pd$fsiteyear) == levels(snails$fsiteyear))
+all(levels(snails$fsiteyear) == levels(factor(rib_occ$fsiteyear)))
+snails$rib_occ <- rib_occ$rib_occ[match(snails$fsiteyear, rib_occ$fsiteyear)]
+pd <- subset(pd, rib_occ == 1)
+snails <- subset(snails, rib_occ == 1)
+pd <- droplevels(pd)
+snails <- droplevels(snails)
+
+# Extract snail density data ---------------------------------------------
+# treat helisoma density in sweeps and seines as a multimethod indicator
+
+names(snails)
+with(snails, {
+  plot(log(1+`HELI_ SW_1`), log(1+`HELI_ SE_1`))
+})
+
+library(reshape2)
+snail_density_df <- melt(snails, 
+                         id.vars=c('AssmtCode_1', 'fsite', 'AssmtYear_1', 
+                                   'region'), 
+                         measure.vars = c('HELI_ SW_1', 'HELI_ SE_1'))
+
+snails$heli_infected <- round(snails$HELDISS3 * snails$HELIRIB_3)
+
+# Extract snail infection data
+snail_inf_df <- snails[!duplicated(snails$AssmtCode_1), 
+                         c('fsite', 'AssmtYear_1', 'region', 
+                           "HELDISS3", "heli_infected")]
+
+# final verification that factor levels are comparable across datasets
+all(levels(pd$fsite) == levels(snails$fsite))
+all(levels(snail_density_df$fsite) == levels(snail_inf_df$fsite))
+all(levels(pd$fsite) == levels(snail_inf_df$fsite))
+
+# create vector of length nsite that describes regional identity
+reg <- pd[!duplicated(pd$fsite), 'region']
+
+# Bundle data for stan ---------------------------------------------
 stan_d <- list(n = nrow(pd), 
-               nsite = max(pd$numsite), 
+               nsite = length(levels(pd$fsite)), 
                nregion = max(pd$region), 
-               site = pd$numsite, 
+               site = as.numeric(pd$fsite), 
                nyear = length(unique(pd$year)),
                year = pd$year - min(pd$year) + 1,
-               region = pd$region, 
+               region = reg, 
                y = pd$rib, 
-               nspec = length(unique(pd$speciescode)),
-               h_spec = as.numeric(pd$speciescode),
-               local_richness = c(scale(pd$local_rich)), 
-               region_richness = c(scale(pd$reg_rich)), 
-               isd = c(scale(log(pd$ISD + .1))))
+               richness = pd$reg_rich, 
+               nsnail = nrow(snail_inf_df),
+               y_snail = snail_inf_df$heli_infected, 
+               k_snail = snail_inf_df$HELDISS3, 
+               site_snail = as.numeric(snail_inf_df$fsite))
 
-watch <- c("eta_site", "eta_region", 
-           "eta_species", "eta_year",
+watch <- c("eta_site", 
+           "eta_year",
            "a0", 
-           "sigma_site", "sigma_region", 
-           "sigma_species", 
+           "sigma_site", 
            "sigma_indiv",
-           "beta_local", "beta_region", "beta_isd", "beta_intxn",
-           "log_lik")
+           "beta_rich", 
+           "mu_p",
+           "p_site",
+           "sigma_p", 
+           "p_region", 
+           "sigma_p_region")
 
 m_init <- stan('R/isd.stan', data=stan_d, chains=1, iter=1, pars=watch)
 m_fit <- stan(fit=m_init, data=stan_d, pars=watch, chains=3, 
               iter=500, cores=3)
 
-traceplot(m_fit, pars=watch[-c(1, length(watch))], inc_warmup=F)
+traceplot(m_fit, pars=watch, inc_warmup=T)
 waic(m_fit)
 
 library(ggmcmc)
 ggd <- ggs(m_fit)
 ggs_caterpillar(ggd, 'eta_year')
-ggs_caterpillar(ggd, 'eta_species')
-ggs_caterpillar(ggd, 'eta_region')
 ggs_caterpillar(ggd, 'eta_site')
+ggs_caterpillar(ggd, 'p_site')
+ggs_caterpillar(ggd, 'p_region')
 
 post <- rstan::extract(m_fit)
 nspec <- stan_d$nspec
