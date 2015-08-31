93298c8f40545e53170df19f806cafd893851ecd
mbjoseph <maxwellbjoseph@gmail.com>
2015-08-31 11:48:34 -0600

    use sitenames instead of site in autocorrelation file

--- R/autocor.R
+++ R/autocor.R
@@ -1,20 +1,25 @@
 # evaluating the site X year autocorrelation as a function of value at time t
 # fitting a simple random effect model to the rib abundance data
-source('R/d_clean.R')
+#source('R/d_clean.R')
 source('R/helpers.R')
 library(rstan)
 library(maps)
 library(scales)
 library(plyr)
+
+pd <- read.csv("data/necropsy20122014_all_rib.csv")
+pd <- subset(pd, speciescode == "PSRE")
+pd <- droplevels(pd)
+
 str(pd)
-summ <- ddply(pd, ~ site + year, summarize, 
+summ <- ddply(pd, ~ sitename + year, summarize, 
               mean_rib = mean(rib))
 plot(x=NULL, y=NULL, 
      xlim=range(log(1 + summ$mean_rib)), ylim=range(log(1 + summ$mean_rib)), 
      xlab='Mean Rib in year t', ylab='Mean Rib in year t + 1')
 abline(0, 1, lty=2)
-for (i in 1:length(unique(summ$site))){
-  sub <- subset(summ, site == unique(summ$site)[i])
+for (i in 1:length(unique(summ$sitename))){
+  sub <- subset(summ, sitename == unique(summ$sitename)[i])
   unique_years <- unique(sub$year)
   if (length(unique_years) > 1){
     sorted_t <- sort(unique_years)
