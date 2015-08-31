2c67706dd144b76311dd53e3e897d08dcd2c85af
mbjoseph <maxwellbjoseph@gmail.com>
2015-08-31 11:47:20 -0600

    adding a simple binomial glm for Rib presence in PSRE as a function of apparent local amphibian host richness

---
+++ R/rib_glm.R
@@ -0,0 +1,18 @@
+# Cleaning the parasite infection data, and
+# defining spatial neighbors
+source('R/d_clean.R')
+
+# unaggregated effect of local richness on rib pr(presence)
+summary_d <- ddply(pd, ~ siteyear, summarize, 
+                   rib_pres = as.numeric(any(rib > 0)), 
+                   local_rich = unique(local_rich), 
+                   site = unique(site), 
+                   year = unique(year))
+plot(jitter(summary_d$local_rich), 
+     jitter(summary_d$rib_pres), 
+     xlab='Local amphibian richness', 
+     ylab='Any PSRE infected')
+
+pres_mod <- glm(rib_pres ~ local_rich, family='binomial', data=summary_d)
+summary(pres_mod)
+
